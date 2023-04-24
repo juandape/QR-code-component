c0dc60a051a453900921bb0f930b2303322f1a5b
Juan David Peña <juandavidp76@hotmail.com>
2023-04-24 17:39:14 -0500

    maquetacion arhivo react

---
+++ src/component-qr/Qr-card.jsx
@@ -0,0 +1,15 @@
+import "./style.css"
+import image from "./assets/image-qr-code.png"
+
+function QrCode() {
+  return (
+    <>
+    <div>
+      <img src={image} alt="qr-code" />
+    </div>
+    
+    </>
+  );
+}
+
+export default QrCode;
---
+++ src/component-text/Text-card.jsx
@@ -0,0 +1,17 @@
+import "./styletext.css";
+
+function Text() {
+  return (
+  <>
+    <div className="card__text">
+      <h2>Improve your front-end skills by building projects</h2>
+      <p>
+        Scan the QR code to visit frontend Mentor and take your coding skills to
+        the next level
+      </p>
+    </div>
+  </>
+  );
+}
+
+export default Text;
